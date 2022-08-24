c9763925642203d07c4afa11c47e5a1e8a841b16
Blessed Oscar <oscarblessed04@gmail.com>
2022-08-24 15:57:18 +0100

    added a few changes to the token authentication

--- api/settings.py
+++ api/settings.py
@@ -37,10 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'funct_api.apps.FunctApiConfig',
     'rest_framework',
     'rest_framework.authtoken',
+    
     'user_api.apps.UserApiConfig',
+    'funct_api.apps.FunctApiConfig',
 ]
 
 MIDDLEWARE = [
